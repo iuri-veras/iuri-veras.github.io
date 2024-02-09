4578bc7f8ef71bc1098b669d4534fe69de61f0a6
Iuri Veras Andrade Lima <iuriveraslima@gmail.com>
2024-02-09 00:07:44 -0300

    Adicionei a primeira seção da página

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ menuIcon.addEventListener('click', () => {
 
 divLinks.addEventListener('animationend', event => {
     if(event.animationName === 'closeMenu'){
-        divLinks.classList.remove('close-menu')
+        divLinks.classList.remove('close-menu');
     }
+})
+
+let divParagraphs = document.querySelectorAll('.paragraphs p');
+let imagens = ['engenharia.jpg', 'redes.jpg', 'programacao.jpg'];
+
+divParagraphs.forEach((paragraph, index) => {
+    paragraph.addEventListener('mouseover', () => {
+        let current = document.querySelector('p.active');
+        if(current !== paragraph){
+            current.classList.remove('active');
+            paragraph.classList.add('active');
+
+            let img = document.getElementById('imgsobremim');
+            img.setAttribute('src', `./image/${imagens[index]}`);
+        }
+    })
 })
