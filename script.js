2cb5e9701192513836ab44d40d47ccba214a9eb6
Iuri Veras Andrade Lima <iuriveraslima@gmail.com>
2024-02-10 23:44:02 -0300

    Terminei de estilizar a segunda seção e adicionei a funcionalidade de scroll da navbar

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 // Barra de navegação
-
 let menuIcon = document.querySelector('.menu-icon');
 let divLinks =  document.querySelector('.links');
 
-menuIcon.addEventListener('click', () => {
-    divLinks.classList.toggle('open-menu');
-
+function animarIconMenu(){
     let lines = document.querySelectorAll('.line');
     
     lines[0].classList.toggle('animate');
     lines[1].classList.toggle('animate');
     lines[2].classList.toggle('animate');
+}
+
+menuIcon.addEventListener('click', () => {
+    divLinks.classList.toggle('open-menu');
+
+    animarIconMenu()
 
     if(!divLinks.classList.contains('open-menu')){
         divLinks.classList.add('close-menu');
     }
 })
 
+// Quando a animação acabar, retira a classe close-menu
 divLinks.addEventListener('animationend', event => {
     if(event.animationName === 'closeMenu'){
         divLinks.classList.remove('close-menu');
     }
+})
+
+// Links da barra de navegação
+let links = document.querySelectorAll('.links a')
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        // Encontra o titulo relacionado ao links
+        let title = document.querySelector(`#${link.id.replace('link-', '')} h2`);
+        let navbar = document.querySelector('nav');
+
+        // Calcula a coordenada y utilizando o tamanho da navbar e a posição atual da tela
+        window.scroll(
+            0, 
+            title.getBoundingClientRect().y - navbar.clientHeight + window.scrollY,
+        );
+
+        if(divLinks.classList.contains('open-menu')){
+            animarIconMenu()
+            divLinks.classList.remove('open-menu');
+            divLinks.classList.add('close-menu');
+        }
+    })
 })
 
 // Sobre mim
@@ -42,7 +69,6 @@ items[0].addEventListener('onscreen', () => {
 })
 
 // Motivos
-
 let btnEsquerda = document.querySelector('button.left');
 let btnDireita = document.querySelector('button.right');
 let circles = document.querySelectorAll('.controllers span')
